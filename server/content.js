d88dbd7be455b3359b3a5c1ef6a8c0a9a6f01532
lassehansen <lahan14@student.sdu.dk>
2016-10-31 12:23:31 +0100

    Added JSON to content file

---
+++ server/content.js
@@ -0,0 +1,19 @@
+var products = [
+	{
+	"Name" : "Computer",
+	"Price" : "5",
+	"Description" : "A good computer",
+	"CreatedDate" : "2016-10-30T18:25:43.511Z",
+	"EndDate" : "2016-12-30T18:25:43.511Z",
+	"Quantity" : "20"
+	
+	},
+	{
+	"Name" : "Water bottle",
+	"Price" : "500",
+	"Description" : "Straight from the himalayas",
+	"CreatedDate" : "2016-10-30T18:25:43.511Z",
+	"EndDate" : "2016-12-30T18:25:43.511Z",
+	"Quantity" : "1"
+	}
+	];
